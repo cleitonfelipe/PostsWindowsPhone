fc3a0cefb05297db8d148aeae61cf02d752da36b
Cleiton Felipe de Moraes <omehe.cleiton@hotmail.com>
2014-03-18 22:55:31 -0300

    Post About Choosers

---
+++ ChoosersShareMind/ChoosersShareMind/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using ChoosersShareMind.Resources;
+using Microsoft.Phone.Tasks;
+
+namespace ChoosersShareMind
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        //TIPO DO OBJETO QUE IREMOS UTILIZAR
+        AddressChooserTask chooser;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            chooser = new AddressChooserTask();
+            chooser.Completed += new EventHandler<AddressResult>(chooser_Completed);
+            chooser.Show();
+        }
+        void chooser_Completed(object sender, AddressResult e)
+        {
+            MessageBox.Show("Nome: " + e.DisplayName + "\nEndereço: " + e.Address);
+        }
+    }
+}
