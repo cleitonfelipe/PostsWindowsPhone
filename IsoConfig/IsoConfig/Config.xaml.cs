5d7a75a416cb5eea5dd5ba9f647b2768fe734c89
Cleiton Felipe de Moraes <omehe.cleiton@hotmail.com>
2014-06-16 22:41:25 -0300

    Post sobre Isolatad Storage

---
+++ IsoConfig/IsoConfig/Config.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO.IsolatedStorage;
+
+namespace IsoConfig
+{
+    public partial class Config : PhoneApplicationPage
+    {
+        IsolatedStorageSettings isoStore = IsolatedStorageSettings.ApplicationSettings;
+        public Config()
+        {
+            InitializeComponent();
+        }
+
+        private void SetSettingValue(string key, string value)
+        {
+            if (!isoStore.Contains(key))
+            {
+                isoStore.Add(key, value);
+            }
+            else
+            {
+                isoStore[key] = value;
+            }
+
+            isoStore.Save();
+        }
+
+        private void RemoveSettingValue(string key)
+        {
+            if (isoStore.Contains(key))
+            {
+                isoStore.Remove(key);
+            }
+            isoStore.Save();
+        }
+
+        private void btnGravar_Click(object sender, RoutedEventArgs e)
+        {
+            this.SetSettingValue("email", txtemail.Text);
+            this.SetSettingValue("twitter", txttwitter.Text);
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            string email, twitter;
+
+            if (isoStore.TryGetValue<string>("email", out email))
+            {
+                txtemail.Text = email;
+            }
+
+            if (isoStore.TryGetValue<string>("twitter", out twitter))
+            {
+                txttwitter.Text = twitter;
+            }
+        }
+
+        private void btnLimpar_Click(object sender, RoutedEventArgs e)
+        {
+            this.RemoveSettingValue("email");
+            this.RemoveSettingValue("twitter");
+
+            txtemail.Text = string.Empty;
+            txttwitter.Text = string.Empty;
+        }
+    }
+}
---
+++ IsoConfig/IsoConfig/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using IsoConfig.Resources;
+using System.IO.IsolatedStorage;
+
+namespace IsoConfig
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        IsolatedStorageSettings isoStore = IsolatedStorageSettings.ApplicationSettings;
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            string twitter;
+            string email;
+
+            if (isoStore.TryGetValue<string>("twitter", out twitter) && isoStore.TryGetValue<string>("email", out email))
+            {
+                valorSetting.Text = twitter;
+                valorSetting2.Text = email;
+            }
+            else 
+            {
+                valorSetting.Text = String.Empty;
+                valorSetting2.Text = String.Empty;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Config.xaml", UriKind.Relative));
+        }
+    }
+}
